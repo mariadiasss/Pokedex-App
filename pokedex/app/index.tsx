337377ca5361427217b41078d6d61e953c016a2d
mariadiasss <mariacoelhodias94@gmail.com>
2025-08-01 12:26:22 -0300

    Parei na pg 34

---
+++ pokedex/app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import Header from '@/components/Header';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Header title="Pokédex"/>
+    </View>
+  );
+}
