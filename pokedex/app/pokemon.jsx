05852b4f5c3be68c2995e5c5657d880c4dcb38a9
mariadiasss <mariacoelhodias94@gmail.com>
2025-08-15 11:55:40 -0300

    fim

--- pokedex/app/pokemon.jsx
+++ pokedex/app/pokemon.jsx
@@ -3,9 +3,8 @@ import { Dimensions, Image, StyleSheet, ScrollView, View } from "react-native";
 import Button from "../components/Button";
 import Header from "../components/Header";
 import TextInfo from "../components/TextInfo";
-import { styles } from "../components/Card/styles";
 
-const widht =Dimensions.get("window").widht;
+const width =Dimensions.get("window").width;
 
 export default function PokemonDetail() {
     const pokemon = useLocalSearchParams();
